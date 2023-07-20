853334c3601d6ae5588eacd5ce9f5bb02d2cd48e
Martin Molcrette <mart1.molcrette@gmail.com>
2023-07-20 16:49:58 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+
+const formatColors = (input) => {
+  if(typeof input !== 'object') return input;
+  if(input.hasOwnProperty('value')) {
+    return {
+      "$type": "color",
+      "$value": input.value
+    };
+  }
+  let res = {};
+  for(let key in input) {
+    res[key] = formatColors(input[key]);
+  }
+  return res;
+};
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
+
+const TextArea = styled.textarea`
+  width: 50%;
+  height: 100%;
+`;
+
+const PreContainer = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Pre = styled.pre`
+  width: 100%;
+  height: 85%;
+  overflow: auto;
+`;
+
+const CopyButton = styled.button`
+  margin: 10px;
+`;
 
 function App() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const outputRef = useRef(null);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+    try {
+      const parsed = JSON.parse(e.target.value);
+      const formatted = formatColors(parsed);
+      setOutput(JSON.stringify(formatted, null, 2));
+    } catch(err) {
+      setOutput('Erreur: Entrée JSON invalide');
+    }
+  };
+
+  const handleCopyClick = () => {
+    if (outputRef.current) {
+      navigator.clipboard.writeText(outputRef.current.textContent)
+        .then(() => alert('JSON copié dans le presse-papiers'))
+        .catch(err => alert('Erreur lors de la copie :', err));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TextArea value={input} onChange={handleInputChange} placeholder="Entrez le JSON ici"></TextArea>
+      <PreContainer>
+        <Pre ref={outputRef}>{output}</Pre>
+        <CopyButton onClick={handleCopyClick}>Copier le JSON</CopyButton>
+      </PreContainer>
+    </Container>
   );
 }
 
